313d91c84f9e853e0420038bbf7e1db5a09e4aa5
knight <karvin_jimenez@hotmail.com>
2024-02-25 20:53:00 -0400

    Project started.

---
+++ core/src/main/java/knight/arkham/Adventure.java
@@ -0,0 +1,45 @@
+package knight.arkham;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.loaders.SkinLoader;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import knight.arkham.screens.GameScreen;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Adventure extends Game {
+    public static Adventure INSTANCE;
+    public OrthographicCamera camera;
+    public Viewport viewport;
+    public int screenWidth;
+    public int screenHeight;
+    public AssetDescriptor<Skin> uiSkin;
+
+    public Adventure() {
+
+        INSTANCE = this;
+    }
+
+    @Override
+    public void create() {
+
+        camera = new OrthographicCamera();
+
+        screenWidth = Gdx.graphics.getWidth();
+        screenHeight = Gdx.graphics.getHeight();
+
+        viewport = new FitViewport(screenWidth / 32f, screenHeight / 32f, camera);
+
+        camera.zoom -= 0.4f;
+
+        camera.position.set(screenWidth / 2f / 32f, screenHeight / 2f / 32f, 0);
+
+        uiSkin = new AssetDescriptor<>("ui/uiskin.json", Skin.class, new SkinLoader.SkinParameter("ui/uiskin.atlas"));
+
+        setScreen(new GameScreen());
+    }
+}
